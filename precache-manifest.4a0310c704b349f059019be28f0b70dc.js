114f1276d186435a69cd961b579386ca759d0f40
Julian Wildmann <julian.wildmann@freenet.de>
2019-08-20 18:06:41 +0200

    Updates

---
+++ precache-manifest.4a0310c704b349f059019be28f0b70dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab138a45bf02ef4bf717c405d04b12d",
+    "url": "/web-shop/index.html"
+  },
+  {
+    "revision": "4c7f566c3cfb56ede511",
+    "url": "/web-shop/static/css/main.2dd27676.chunk.css"
+  },
+  {
+    "revision": "b4f5b3b3cb1e548ade9c",
+    "url": "/web-shop/static/js/2.9bfc9976.chunk.js"
+  },
+  {
+    "revision": "4c7f566c3cfb56ede511",
+    "url": "/web-shop/static/js/main.28911d08.chunk.js"
+  },
+  {
+    "revision": "8e303716fa81f179c154",
+    "url": "/web-shop/static/js/runtime~main.8a5ca21b.js"
+  }
+]);
