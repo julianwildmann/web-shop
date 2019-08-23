acc7d38bf09bddff07815f4a09ffa2b73284e5a4
Julian Wildmann <julian.wildmann@freenet.de>
2019-08-23 08:13:33 +0200

    contextualimage

--- src/components/ContextualImage.js
+++ src/components/ContextualImage.js
@@ -8,7 +8,7 @@ class Image extends React.Component {
         return (
             <div>
                 <div className="Image">
-                    <img src="media/ContextualImage2.jpg" alt="Eames Chair DSW" width="100%" height="auto" />
+                    <img src="media/ContextualImage.jpg" alt="Eames Chair DSW" width="100%" height="auto" />
                 </div>
                 <div className="ReturnLinkProductPage">
                     <a href="https://lmubwl.eu.qualtrics.com/jfe/form/SV_d4HggX2AmuSSay1">Zurück zur Umfrage</a>
