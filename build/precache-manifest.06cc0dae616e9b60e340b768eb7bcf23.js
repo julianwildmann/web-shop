61034229dd70df68d3cd9f8c33556e83ffd00a17
Julian Wildmann <julian.wildmann@freenet.de>
2019-08-26 23:54:27 +0200

    ProductImage

---
+++ build/precache-manifest.06cc0dae616e9b60e340b768eb7bcf23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f28e96e85c3f4205379aa1a31fddf8",
+    "url": "/web-shop/index.html"
+  },
+  {
+    "revision": "9cca0a49ba25b096481d",
+    "url": "/web-shop/static/css/main.2dd27676.chunk.css"
+  },
+  {
+    "revision": "b4f5b3b3cb1e548ade9c",
+    "url": "/web-shop/static/js/2.9bfc9976.chunk.js"
+  },
+  {
+    "revision": "9cca0a49ba25b096481d",
+    "url": "/web-shop/static/js/main.815993f0.chunk.js"
+  },
+  {
+    "revision": "8e303716fa81f179c154",
+    "url": "/web-shop/static/js/runtime~main.8a5ca21b.js"
+  }
+]);
