370102267352bc57b0754e45727df9c25c02054d
Julian Wildmann <julian.wildmann@freenet.de>
2019-08-27 00:29:34 +0200

    productimage2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-shop/precache-manifest.710d84d998fbe660548ab66a5bb1ba91.js"
+  "/web-shop/precache-manifest.5a654a8fb4b515f9994934b84e393d1c.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/components/Image.js
+++ src/components/Image.js
@@ -8,7 +8,7 @@ class Image extends React.Component {
         return (
             <div>
                 <div className="ContextualImage">
-                    <img src="media/ProductImage.jpg" alt="Eames Chair DSW" width="100%" height="auto" />
+                    <img src="media/ProductImage2.jpg" alt="Eames Chair DSW" width="100%" height="auto" />
                 </div>
                 <div className="ReturnLinkProductPage">
                     <a href="https://lmubwl.eu.qualtrics.com/jfe/form/SV_d4HggX2AmuSSay1">Zurück zur Umfrage</a>
